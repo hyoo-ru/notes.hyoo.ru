0375d788e4f8e6b264b0f7f371d0b103c19f8ec1
КарловскийДА <kard@1c.ru>
2019-09-03 21:36:12 +0300

    initial

---
+++ notes.view.ts
@@ -0,0 +1,186 @@
+namespace $.$$ {
+	
+	export class $hyoo_notes extends $.$hyoo_notes {
+
+		pages() {
+			return [
+				this.Tags_page() ,
+				this.Notes_page() ,
+				... this.note() ? [ this.Note_page() ] : [] ,
+				... this.tagging() ? [ this.Tagging_page() ] : [] ,
+			]
+		}
+
+		note_ids( next? : string[] ) {
+			return this.$.$mol_state_local.value( 'note_ids' , next ) || []
+		}
+		
+		note_tags( id : string , next? : string[] ) {
+			return this.$.$mol_state_local.value( `note=${id}.tags` , next ) || []
+		}
+		
+		note( next? : string | null ) {
+			return this.$.$mol_state_arg.value( 'note' , next )
+		}
+
+		note_content( note : string , next? : string ) {
+			if( next ) this.note_ids([ note , ... this.note_ids().filter( id => id !== note ) ])
+			return this.$.$mol_state_local.value( `note=${ note }.content` , next ) || ''
+		}		
+		
+		note_current_content( next? : string ) {
+			return this.note_content( this.note() , next )
+		}		
+		
+		tag_ids( next? : string[] ) {
+			return this.$.$mol_state_local.value( 'tag_ids' , next ) || []
+		}
+		
+		tag( next? : string | null ) {
+			return this.$.$mol_state_arg.value( 'tag' , next )
+		}
+
+		tagging( next? : boolean ) {
+			return this.$.$mol_state_arg.value( 'tagging' , next === undefined ? undefined : next ? '' : null ) === ''
+		}
+
+		tag_add_showed() {
+			const id = this.tag_filter()
+			if( !id ) return false
+			if( this.tag_ids().includes( id ) ) return false
+			return true
+		}
+
+		tags_body() {
+			return [
+				this.Tag_filter() ,
+				... this.tag_add_showed() ? [ this.Tag_add() ] : [] ,
+				this.Tag_list() ,
+			]
+		}
+
+		tagging_add_showed() {
+			const id = this.tagging_filter()
+			if( !id ) return false
+			if( this.tag_ids().includes( id ) ) return false
+			return true
+		}
+
+		tagging_body() {
+			return [
+				this.Tagging_filter() ,
+				... this.tagging_add_showed() ? [ this.Tagging_add() ] : [] ,
+				this.Tagging_list() ,
+				this.Note_drop() ,
+			]
+		}
+
+		notes_filter_showed() {
+			return this.note_ids_available().length > 5
+		}
+
+		notes_body() {
+			return [
+				... this.notes_filter_showed() ? [ this.Note_filter() ] : [] ,
+				this.Notes_list() ,
+				this.Tag_drop() ,
+			]
+		}
+
+		tag_add() {
+			const id = this.tag_filter()
+			this.tag_ids([ id , ... this.tag_ids() ])
+			this.tag( id )
+			this.tag_filter( '' )
+		}
+
+		tagging_add() {
+			const id = this.tagging_filter()
+			this.tag_ids([ id , ... this.tag_ids() ])
+			this.tagging_tag( id , true )
+			this.tagging_filter( '' )
+		}
+
+		tag_title( id : string ) {
+			return id
+		}
+
+		id( id : string ) {
+			return id
+		}
+
+		note_title( id : string ) {
+			return this.note_content( id ).replace( /\n[\s\S]*/ , ''  ) || id
+		}
+
+		note_current_title() {
+			return this.note_title( this.note() )
+		}
+
+		tag_rows() {
+			return this.tag_ids()
+			.filter( $mol_match_text( this.tag_filter() , id => [ id ] ) )
+			.map( id => this.Tag_row( id ) )
+		}
+		
+		tagging_rows() {
+			return this.tag_ids()
+			.filter( $mol_match_text( this.tagging_filter() , id => [ id ] ) )
+			.map( id => this.Tagging_tag_row( id ) )
+		}
+		
+		note_ids_available() {
+
+			const tag = this.tag()
+			if( !tag ) return this.note_ids()
+			
+			return this.note_ids()
+			.filter( note => this.note_tags( note ).includes( tag ) )
+
+		}
+		
+		note_rows() {
+			return this.note_ids_available()
+			.filter( $mol_match_text( this.note_filter() , id => [ this.note_content( id ) ] ) )
+			.map( id => this.Note_row( id ) )
+		}
+		
+		note_add() {
+			const id = $mol_stub_code()
+			this.note_ids([ id , ... this.note_ids() ])
+			this.note_tags( id , $mol_maybe( this.tag() ) )
+			this.note( id )
+			this.Note_content().Edit().focused( true )
+		}
+
+		note_drop() {
+			const note = this.note()
+			this.note_ids( this.note_ids().filter( id => id !== note ) )
+			this.note_tags( note , null )
+			this.note_content( note , null )
+			this.note( null )
+			this.tagging( false )
+		}
+
+		notes_title() {
+			return this.tag_title( this.tag() ) || super.notes_title()
+		}
+
+		tagging_tag( tag : string , next?: boolean ) {
+			
+			const note = this.note()
+			const tags = this.note_tags( note )
+
+			if( next === undefined ) return tags.includes( tag )
+			
+			const filtered = tags.filter( id => id !== tag )
+			
+			if( next ) this.note_tags( note , [ tag , ... filtered ] )
+			else this.note_tags( note , filtered )
+
+			return next
+		}
+		
+	}
+	
+}
