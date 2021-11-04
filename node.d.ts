457ee528da6d16a4ee36524db7128301ddd3a496
nin-jin <nin-jin@users.noreply.github.com>
2021-11-04 07:18:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2967,6 +2967,7 @@ declare namespace $ {
         type(val?: any): string;
         link(val?: any): string;
         title(val?: any): string;
+        Image(): $mol_image;
     }
 }
 
--- web.d.ts
+++ web.d.ts
@@ -2978,6 +2978,7 @@ declare namespace $ {
         type(val?: any): string;
         link(val?: any): string;
         title(val?: any): string;
+        Image(): $mol_image;
     }
 }
 
