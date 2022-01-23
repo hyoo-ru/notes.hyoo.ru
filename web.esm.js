524a8f903871aa5f8de87fdb7e2bcdb38a561f6a
nin-jin <nin-jin@users.noreply.github.com>
2022-01-23 07:09:27 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -840,6 +840,8 @@ var $;
             this.sub_from = from;
         }
         affect(quant) {
+            if (this.cursor === $mol_wire_cursor.final)
+                return false;
             if (this.cursor >= quant)
                 return false;
             this.cursor = quant;
@@ -2718,8 +2720,11 @@ var $;
         }
     }
     $.$mol_after_work = $mol_after_work;
+    if (typeof requestIdleCallback !== 'function') {
+        $.$mol_after_work = $mol_after_timeout;
+    }
 })($ || ($ = {}));
-//mol/after/work/work.web.ts
+//mol/after/work/work.ts
 ;
 "use strict";
 var $;
@@ -10573,6 +10578,9 @@ var $;
         ], $mol_text.prototype, "rows", null);
         __decorate([
             $mol_mem_key
+        ], $mol_text.prototype, "header_content", null);
+        __decorate([
+            $mol_mem_key
         ], $mol_text.prototype, "cell_contents", null);
         __decorate([
             $mol_mem_key
@@ -10586,12 +10594,6 @@ var $;
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "table_cell_content", null);
-        __decorate([
-            $mol_mem_key2
-        ], $mol_text.prototype, "text2spans", null);
-        __decorate([
-            $mol_mem_key2
-        ], $mol_text.prototype, "code2spans", null);
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "block_content", null);
