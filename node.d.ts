c0cadc1aad7b792e48ad97e4e9108e26077c4b18
nin-jin <nin-jin@users.noreply.github.com>
2022-11-12 07:11:38 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -574,6 +574,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_wire_solid(): void;
+}
+
+declare namespace $ {
     function $mol_const<Value>(value: Value): {
         (): Value;
         '()': Value;
@@ -2174,6 +2178,26 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    class $mol_format extends $mol_string {
+        allow(): string;
+        hint(): string;
+        keyboard(): string;
+        mask(id: any): string;
+    }
+}
+
+declare namespace $ {
+}
+
+declare namespace $.$$ {
+    class $mol_format extends $.$mol_format {
+        selection([from, to]?: [number, number]): number[];
+        value_changed(next?: string): string;
+        event_change(next?: InputEvent): void;
+    }
+}
+
+declare namespace $ {
     class $mol_icon_chevron extends $mol_icon {
         path(): string;
     }
@@ -2298,9 +2322,9 @@ declare namespace $ {
         value_number(val?: any): number;
         value_moment(val?: any): $mol_time_moment;
         value(val?: any): string;
-        input_hint(): string;
+        input_mask(id: any): string;
         enabled(): boolean;
-        Input(): $$.$mol_string;
+        Input(): $$.$mol_format;
         month_moment(): $mol_time_moment;
         day_selected(id: any): boolean;
         day_click(id: any, event?: any): any;
@@ -2333,6 +2357,7 @@ declare namespace $ {
 declare namespace $.$$ {
     class $mol_date extends $.$mol_date {
         trigger_content(): string[];
+        input_mask(val: string): "____-__-__ __:__" | "____-__-__ ";
         value(val?: string): string;
         value_moment(val?: $mol_time_moment): $mol_time_moment;
         month_moment(next?: $mol_time_moment): $mol_time_moment;
