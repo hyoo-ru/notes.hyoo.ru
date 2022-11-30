1cc849228ee055ba60594fa1b1a947d1347fd53f
nin-jin <nin-jin@users.noreply.github.com>
2022-11-30 07:10:39 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -5484,8 +5484,7 @@ var $;
         event() {
             return {
                 ...super.event(),
-                input: (event) => this.event_change(event),
-                keydown: (event) => this.event_key_press(event)
+                input: (event) => this.event_change(event)
             };
         }
         plugins() {
@@ -5541,11 +5540,6 @@ var $;
                 return event;
             return null;
         }
-        event_key_press(event) {
-            if (event !== undefined)
-                return event;
-            return null;
-        }
         submit(event) {
             if (event !== undefined)
                 return event;
@@ -5571,9 +5565,6 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_string.prototype, "event_change", null);
-    __decorate([
-        $mol_mem
-    ], $mol_string.prototype, "event_key_press", null);
     __decorate([
         $mol_mem
     ], $mol_string.prototype, "submit", null);
--- web.d.ts
+++ web.d.ts
@@ -1688,7 +1688,6 @@ declare namespace $ {
         };
         event(): {
             input: (event?: any) => any;
-            keydown: (event?: any) => any;
         };
         plugins(): readonly any[];
         selection_watcher(): any;
@@ -1705,7 +1704,6 @@ declare namespace $ {
         length_max(): number;
         type(val?: any): string;
         event_change(event?: any): any;
-        event_key_press(event?: any): any;
         submit(event?: any): any;
         Submit(): $$.$mol_hotkey;
     }
