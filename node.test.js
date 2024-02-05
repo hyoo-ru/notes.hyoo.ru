dbe898f25d2373f39d456997c51402fa374b87af
nin-jin <nin-jin@users.noreply.github.com>
2024-02-05 07:10:21 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -10507,7 +10507,7 @@ var $;
     function $mol_offline() { }
     $.$mol_offline = $mol_offline;
 })($ || ($ = {}));
-//mol/offline/offline.node.ts
+//mol/offline/offline.ts
 ;
 "use strict";
 var $;
--- web.d.ts
+++ web.d.ts
@@ -2852,6 +2852,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_offline_web(): void;
+}
+
+declare namespace $ {
 }
 
 declare namespace $ {
